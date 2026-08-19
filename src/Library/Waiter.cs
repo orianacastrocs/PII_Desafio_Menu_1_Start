699ab959113dedba3fa13738338a48686e90dd5a
Matias Ubbriaco <mhubb923@gmail.com>
2026-08-19 19:09:57 -0300

    creacion clase Waiter

--- src/Library/Waiter.cs
+++ src/Library/Waiter.cs
@@ -13,6 +13,22 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Waiter
     {
+        public string Name { get; set; }
         private ArrayList assignedTables = new ArrayList();
+
+        public void AssignTable(Table table)
+        {
+            assignedTables.Add(table);
+        }
+        public void  TakeOrder(Table table, Dish dish)
+        {
+            table.AddToOrder(dish);
+
+        }
+        public Waiter(string name = "")
+        {
+            this.Name = name;
+
+        }
     }
 }
