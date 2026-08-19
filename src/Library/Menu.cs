d4359d7b41aed291fd27f66f9319b73c6c474728
valenavarrine <valenavarrine@gmail.com>
2026-08-19 19:30:26 -0300

    Correcion Menu

--- src/Library/Menu.cs
+++ src/Library/Menu.cs
@@ -13,11 +13,31 @@ namespace Ucu.Poo.Restaurant
     /// Representa el conjunto de platillos <see cref="Dish"/> disponibles en el
     /// restaurante.
     /// </summary>
-    public class Menu
+public class Menu
     {
         private ArrayList dishes = new ArrayList();
 
+        public void AddDish(Dish dish)
+        {
+            this.dishes.Add(dish);
+        }
 
+        public void RemoveDish(Dish dish)
+        {
+            this.dishes.Remove(dish);
+        }
+
+        public Dish GetDishByName(string name)
+        {
+            foreach (Dish dish in this.dishes)
+            {
+                if (dish.Name == name)
+                {
+                    return dish;
+                }
+            }
+            return null;
+        }
     }
 }
 
