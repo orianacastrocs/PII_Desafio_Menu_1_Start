dc4c5e22f75837b7de9573ac16b300d382a5ce1e
thiago <thiagogaraycochea@gmail.com>
2026-08-19 18:41:21 -0300

    un padre nuestro y funciona

--- src/Library/Table.cs
+++ src/Library/Table.cs
@@ -13,7 +13,35 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Table
     {
+        // 1. Responsabilidades de conocer (Faltaban)
+        public int Number { get; private set; }
+        public bool IsOccupied { get; private set; }
+
         private ArrayList order = new ArrayList();
+
+        // 2. Constructor: Permite inicializar las propiedades en una sola operación (Faltaba)
+        public Table(int number)
+        {
+            this.Number = number;
+            this.IsOccupied = false;
+        }
+
+        // 3. Responsabilidades de hacer (Faltaban)
+        public void Occupy()
+        {
+            this.IsOccupied = true;
+        }
+
+        public void Free()
+        {
+            this.IsOccupied = false;
+            this.order.Clear(); // Vacía la lista usando Clear() como dice el tip
+        }
+
+        public void AddToOrder(Dish dish)
+        {
+            this.order.Add(dish);
+        }
 
         public bool HasOrders()
         {
